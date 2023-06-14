d27f41ff77f9724cac02b38ad341b9175db337c7
SibabalweMvunelwa <sibabalwemvunelwa@gmail.com>
2023-06-14 05:10:37 +0200

    DWA5 - Working on it

--- DWA5/challenge1/scripts.js
+++ DWA5/challenge1/scripts.js
@@ -30,31 +30,38 @@ const result = document.querySelector("[data-result]");
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const entries = new FormData(event.target);
-    const { dividend, divider } = Object.fromEntries(entries);
   
     try{
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
     if (dividend=='' || divider=='') {
 
      throw  'Division not performed. Both values are required in inputs. Try again'
      result.innerText = 'Division not performed. Both values are required in inputs. Try again'
 
-  } else if ( divider<0 ) {
+  } 
+  
+     if ( divider<0 ) {
 
     
     result.innerText = 'Division not performed. Invalid number provided. Try again'
     console.error ('Division not performed. Invalid number provided. Try again')
 
-  } else if ( (Number.isNaN(Number(dividend)) ) && (Number.isNaN(Number(divider))) ) {
+  } 
+  
+    if ( (Number.isNaN(Number(dividend)) ) && (Number.isNaN(Number(divider))) ) {
 
      result.innerText = 'Something critical went wrong. Please reload the page'
      console.error ('Something critical went wrong. Please reload the page')
 
-  } else {
+    }
     
-    result.innerText = Math.floor(dividend / divider)};
-} catch {
+    result.innerText = Math.floor(dividend / divider);
 
+  } catch {
 
+    result.innerText = 'Division not performed. Invalid number provided. Try again';
+    console.log(err.stack);
 }  
-  });
+});
