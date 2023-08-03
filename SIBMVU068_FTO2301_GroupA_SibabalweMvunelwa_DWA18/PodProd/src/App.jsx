9e2fff0adb13492194f4979a4cde7eac660169a1
SibabalweMvunelwa <sibabalwemvunelwa@gmail.com>
2023-08-03 23:26:15 +0200

    DWA18: Added Loadstates

--- SIBMVU068_FTO2301_GroupA_SibabalweMvunelwa_DWA18/PodProd/src/App.jsx
+++ SIBMVU068_FTO2301_GroupA_SibabalweMvunelwa_DWA18/PodProd/src/App.jsx
@@ -10,12 +10,33 @@ import './App.css';
 import { supabase } from './supabaseClient'
 import Auth from './Auth'
 import Account from './Account'
+import { LinearProgress, Container, Box, CircularProgress } from '@mui/material';
 
 const App = () => {
+
+  const isValidJson = (string) => {
+    if (typeof string != 'string') {
+      return false;
+    }
+    try {
+      JSON.parse(string);
+      return true;
+    } catch (error) {
+        return false;
+    }
+  }
+
+  if (!isValidJson(localStorage.getItem('currentEpisode'))) {
+    localStorage.clear();
+  }
+
   const [podcasts, setPodcasts] = useState([]);
-  const [currentEpisode, setCurrentEpisode] = useState(localStorage.getItem('currentEpisode') ? JSON.parse(localStorage.getItem('currentEpisode')) : null);
+  const [currentEpisode, setCurrentEpisode] = useState(localStorage.getItem('currentEpisode') ?  (JSON.parse(localStorage.getItem('currentEpisode'))) : null);
   const [currentSeasonTitle, setCurrentSeasonTitle] = useState(localStorage.getItem('currentSeasonTitle') ?? null);
   const [currentSeasonImage, setCurrentSeasonImage] = useState(localStorage.getItem('currentSeasonImage') ?? null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  
   
   const handleCurrentSeasonImage = (image) => {
     setCurrentSeasonImage(image)
@@ -45,38 +66,51 @@ const App = () => {
 
 
   useEffect(() => {
+    setIsLoading(true);
     // Fetch data from the API endpoint
     axios.get('https://podcast-api.netlify.app/shows')
-      .then(response => setPodcasts(response.data))
+      .then(response => {
+        setPodcasts(response.data)
+        setIsLoading(false);
+      })
       .catch(error => console.error(error));
   }, []);
 
-  return (
-    <div>
-      {/* Supabase */}
-      <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
-    </div>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<List podcasts={podcasts}></List>} />
-        <Route path="/podcast/:id" 
-               element={
-                        <Details handleCurrentEpisode={handleCurrentEpisode} 
-                                 handleCurrentSeasonTitle={handleCurrentSeasonTitle}
-                                 handleCurrentSeasonImage={handleCurrentSeasonImage}
-                                 >
-                        </Details>
-                        } />
-      </Routes>
-    </BrowserRouter>
-    {/* // Podcast Player goes here, as it needs to persist regardless of route */}
-    <Player currentEpisode={currentEpisode}
-            currentSeasonTitle={currentSeasonTitle}
-            currentSeasonImage={currentSeasonImage}>
-    </Player>
-    </div>
-  );
+  if (isLoading) {
+    return (
+      <div>
+        <p>LOADING...</p>
+        <LinearProgress />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {/* Supabase
+        <div className="container" style={{ padding: '50px 0 100px 0' }}>
+        {!session ? <Auth /> : <Account key={session.user.id} session={session} />} */}
+      {/* </div> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<List podcasts={podcasts} setPodcasts={setPodcasts}></List>} />
+          <Route path="/podcast/:id" 
+                element={
+                          <Details handleCurrentEpisode={handleCurrentEpisode} 
+                                  handleCurrentSeasonTitle={handleCurrentSeasonTitle}
+                                  handleCurrentSeasonImage={handleCurrentSeasonImage}
+                                  >
+                          </Details>
+                          } />
+        </Routes>
+      </BrowserRouter>
+      {/* // Podcast Player goes here, as it needs to persist regardless of route */}
+      <Player currentEpisode={currentEpisode}
+              currentSeasonTitle={currentSeasonTitle}
+              currentSeasonImage={currentSeasonImage}>
+      </Player>
+      </div>
+    );
+  }
 };
 
 
--- SIBMVU068_FTO2301_GroupA_SibabalweMvunelwa_DWA18/PodProd/src/Components/EpisodeDetails.jsx
+++ SIBMVU068_FTO2301_GroupA_SibabalweMvunelwa_DWA18/PodProd/src/Components/EpisodeDetails.jsx
@@ -1,112 +1,118 @@
 /* eslint-disable */
 
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import  FavoriteIcon from '@mui/icons-material/Favorite';
-import { Accordion, AccordionDetails, AccordionSummary, Card, Container, List, ListItem, ListItemText, CardMedia, Button } from '@mui/material';
-import Fuse from 'fuse.js'
+import { Accordion, AccordionDetails, AccordionSummary, Card, Container, List, ListItem, ListItemText, CardMedia, Button, IconButton } from '@mui/material';
+import { House } from '@mui/icons-material';
 
 const Details = ({handleCurrentEpisode, handleCurrentSeasonTitle, handleCurrentSeasonImage}) => {
   const [podcast, setPodcast] = useState(null);
   const [params, setParams] = useState(useParams())
+  const [isLoading, setIsLoading] = useState(false)
 
 // Redirect the user to the "/podcast" URL
-    const navigate = useNavigate();
-    const handleRedirectHome = () => {
-          navigate('/')
-          }
+  const navigate = useNavigate();
+  const handleRedirectHome = () => {
+        navigate('/')
+  }
+  const formatDate = (date) => {
+    let formatted = new Date(date);
+    return formatted.toLocaleString();
+  }
+  
 
   const handlePlay = (episode, title, image) => {
     handleCurrentEpisode(episode);
     handleCurrentSeasonTitle(title);
     handleCurrentSeasonImage(image);
-    if (episode !== null && typeof episode === 'object') {
-        localStorage.setItem('currentEpisode', JSON.stringify(episode));
-      } else {
-        localStorage.setItem('currentEpisode', episode);
-      };
+    localStorage.setItem('currentEpisode', JSON.stringify(episode));
     localStorage.setItem('currentSeasonTitle', title);
     localStorage.setItem('currentSeasonImage', image)
     localStorage.setItem('currentTime', 0);
-    console.log(localStorage)
   }
 
   useEffect(() => {
+    console.log('loading')
+    setIsLoading(true);
     // Fetch data for a single podcast using the show's ID from the URL parameter
     axios.get(`https://podcast-api.netlify.app/id/${params.id}`)
-      .then(response => setPodcast(response.data))
-      .catch(error => console.error(error));
+      .then(response => {
+        console.log(response.data)
+        setIsLoading(false)
+        setPodcast(response.data)
+      })
+      .catch(error => {
+        setIsLoading(false)
+        console.error(error)
+      });
   }, [params.id]);
 
-  if (!podcast) {
+  if (!podcast || isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="md" sx={{marginBottom: '50%'}}>
         {/* Home button */}
-        <Button variant="outlined" onClick={() => handleRedirectHome()}> Home </Button>
-        
-        <Card>
+        <IconButton sx={{border: '1pt solid grey'}} aria-label='Home' size='large' onClick={() => handleRedirectHome()}>
+          <House fontSize='large'></House>
+        </IconButton>
+        {/* <Button variant="outlined" onClick={() => handleRedirectHome()}> Home </Button> */}
 
-        <CardMedia
-                component="img"
-                sx={{ 
-                      width: '50%', 
-                      display: 'flex' ,
-                      flexDirection: 'column' ,
-                      alignItems: 'center' ,
-                      justifyContent: 'center' 
-                    }}
-                image={ podcast.image }
-                alt="podcast image" 
-            /> 
-
-        <h2>{podcast.title}</h2>
-        <h3>Seasons: {podcast.seasons.length}</h3>
-    
-        <h3>{podcast.description}</h3>
-        <h3>{podcast.genres.join(", ")}</h3>
-        <h3>Last update: {podcast.updated}</h3>
-        
-        <div>
-            {podcast.seasons.map(season => (
-                <Accordion>
-                    <AccordionSummary>
-                        Season { season.season }
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    {/* Season card here with image and other available details */}
-                    <Card>
-                    {/* <CardMedia
-                            component="img"
-                            sx={{ 
-                                  width: '50%', 
-                                  display: 'flex' ,
-                                  flexDirection: 'column' ,
-                                  alignItems: 'center' ,
-                                  justifyContent: 'center' 
-                                }}
-                            image={ season.image }
-                            alt="Season image" 
-                        />  */}
-                    <h2>{season.title}</h2>
-                    <h3>Episodes: {season.episode}</h3>
-                    </Card>
-                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                        {season.episodes.map(episode => (
-                            <ListItem key={episode.id} sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                                <ListItemText primary={episode.title} secondary={"Episode: " + episode.episode} />
-                                <Button variant="outlined" onClick={() => handlePlay(episode, season.title, season.image)}> Play </Button>
-                                <Button variant="outlined" >{ <FavoriteIcon /> } </Button>
-                            </ListItem>
-                        ))}
-                    </List>
-                    </AccordionDetails>
-                </Accordion>
-            ))}
-        </div>
+        <Card
+        sx = {{ marginTop: '10%'}}>
+          <CardMedia
+                  component="img"
+                  sx={{
+                        margin: 'auto', 
+                        width: '50%', 
+                      }}
+                  image={ podcast.image }
+                  alt="podcast image" 
+              /> 
+          <h2>{podcast.title}</h2>
+          <h3>Seasons: {podcast.seasons.length}</h3>
+      
+          <h3>{podcast.description}</h3>
+          <h3>{podcast.hasOwnProperty('genres') && podcast.genres.length > 0 ? podcast.genres.join(", ") : 'No Genres'}</h3>
+          <h3>Last update: {formatDate(podcast.updated)}</h3>
+          
+          <div>
+              {podcast.seasons.map(season => (
+                  <Accordion>
+                      <AccordionSummary>
+                          Season { season.season }
+                      </AccordionSummary>
+                      <AccordionDetails>
+                      <Card>
+                        <CardMedia
+                          component="img"
+                          sx={{
+                                margin: 'auto', 
+                                width: '50%', 
+                              }}
+                          image={ season.image }
+                          alt="season image" 
+                        /> 
+                        <h2>{season.title}</h2>
+                        <h3>Episodes: {season.episodes.length}</h3>
+                      </Card>
+                      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                          {season.episodes.map(episode => (
+                              <ListItem key={episode.id} sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width:'100%'}}>
+                                  <ListItemText primary={episode.title} secondary={"Episode: " + episode.episode} />
+                                  <Button variant="outlined" onClick={() => handlePlay(episode, season.title, season.image)}> Play </Button>
+                                  <Button variant="outlined" >{ <FavoriteIcon /> } </Button>
+                              </ListItem>
+                          ))}
+                      </List>
+                      </AccordionDetails>
+                  </Accordion>
+              ))}
+          </div>
         </Card>
     </Container>
   );
